3765a1fd8c73a790d3f6a3c910bf8f852f3fc3f1
Tarun Kashyap <tarunk05151999@gmail.com>
2025-08-02 10:28:14 +0530

    Initial Commit

---
+++ tarun/src/main/java/com/example/tarun/TarunApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarunApplication.class, args);
+	}
+
+}
---
+++ tarun/src/main/java/com/example/tarun/controller/GetController.java
@@ -0,0 +1,13 @@
+package com.example.tarun.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
